e335e0bc5638efd0ae2ae393d678227754f5b2e6
kangxunchen <2650224868@qq.com>
2026-06-24 09:32:12 +0800

    feat: 插件集成

---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+// uno.config.ts，支持 HBuilderX
+import { defineConfig } from 'unocss'
+import { presetUni } from '@uni-helper/unocss-preset-uni'
+
+export default defineConfig({
+  presets: [
+    presetUni()
+  ]
+})
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import Uni from '@uni-helper/plugin-uni'
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    uni(),
-  ],
+    Uni(),
+    UnoCSS(),
+    AutoImport({
+      imports: ['vue', 'uni-app', 'pinia'],
+      dts: './auto-imports.d.ts'
+    })
+  ]
 })
